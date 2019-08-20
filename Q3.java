9df26955589ae95901be4f07565e03b3efed5914
pragya-sharma <45152090+pragya-sharma@users.noreply.github.com>
2019-08-21 00:08:14 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -8,7 +8,13 @@ public class Q3{
      Scanner in = new Scanner(System.in);
      int n1 = in.nextInt();
      int n2 = in.nextInt();
-     System.out.println(n1+" + "+n2+" = " +(n1+n2));
+    System.out.println(n1+" + "+n2+" = " +(n1+n2));
+    System.out.println(n1+" - "+n2+" = " +(n1-n2));
+    System.out.println(n1+" + "+n2+" = " +(n1+n2));
+    System.out.println(n1+" * "+n2+" = " +(n1*n2));
+    System.out.println(n1+" + "+n2+" = " +(n1/n2));
+    System.out.println(n1+" % "+n2+" = " +(n1%n2));
+     
   }
 }
      
